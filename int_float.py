1ce9fc48f80389fddb01a0b5bac0ea66dce25a17
adahan77 <adahan2894@gmail.com>
2022-11-17 19:15:51 +0600

    слайды

---
+++ int_float.py
@@ -0,0 +1,73 @@
+print(' Числовые типы данных')
+print('problem 1')
+x = 17925<=(34**2)
+y = 17925<=(26*3)
+z = 17925<=(17*33)
+s = 17925<=(4394*4)
+print(x,y,z,s)
+
+print('problem 2')
+e = 7%3*4.8
+print(e)
+r = 5
+d = 5
+t = d==r
+print(t)
+g = 3
+u = 5
+i = g<u
+print(i)
+
+print('problem3')
+a = (8+2)*8**2-193432
+print(a)
+
+
+print('problem4')
+v = (17*3)<(12*5)
+q = (12**3)>(13*7)
+k = (4**5)==(512+512)
+print(v,q,k)
+
+
+print('problem5')
+print(-21//10)
+
+print('problem6')
+l = int(input('Введите прожитые годы: '))
+j = int(input('Введите сколько нужно: '))
+print((l/j)*100)
+
+print('problem7')
+o = 2005
+p = 2020
+print(p-o+2, p-2-o)
+
+
+print('problem8')
+print((25+75+10+95)/4)
+
+print('problem9')
+
+f=4
+h=4
+m=4
+print((f+h)*m)
+
+
+print('problem10')
+ab=2
+ac=6
+av=15/2
+af=1
+ae=4
+
+print('problem11')
+aw=2+3
+ar=aw*3
+ah=ar**2
+al=ah-193432
+ak=5
+aq=(aw-ar**(ah/al))%ak
+print(al)
+print(aq)
